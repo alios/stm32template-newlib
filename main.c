fa8cdd995fff5de3cfe03a11b6b520e3014bb285
Markus Barenhoff <alios@alios.org>
2010-03-25 09:53:38 +0100

    - removed test code from main()

--- main.c
+++ main.c
@@ -3,19 +3,6 @@
 
 int main(void)
 {
-  int i = 1;
-  char* cs = NULL;
-  const size_t SIZE = 1024;
-
-  for(;;) {
-    if((cs = malloc(SIZE)) == NULL)
-      {
 	for(;;);
-      }
-    printf("hello world! this is iteration %d\n", i);
-    snprintf(cs, SIZE, "hello buffer! this is iteration %d\n", i++);
-
-    free(cs);
-  }
 }
 
