4fdd2b459e167df21dd8f0c022ba5797897eff3b
Markus Barenhoff <alios@alios.org>
2010-09-22 15:48:39 +0200

    added heap peak counter to sbrk_r

--- crt/crt1.c
+++ crt/crt1.c
@@ -9,10 +9,12 @@ extern int8_t _sheap;
 extern int8_t _eheap;
 static const int8_t* eheap_p = &_eheap;
 static const int8_t* sheap_p = &_sheap;
-
 static intptr_t heap_ptr;
 
+size_t heap_peak = 0x0;
 
+
+/*
 long _write_r(struct _reent *_r, int fd, const void *buf, int cnt)
 {
   _r->_errno = EBADF;
@@ -59,6 +61,8 @@ int _isatty( struct _reent *_r, int fd)
   _r->_errno = ENOTTY;
   return 0;
 }
+*/
+
 
 void *_sbrk_r(struct _reent *r, intptr_t incr)
 {
@@ -72,8 +76,11 @@ void *_sbrk_r(struct _reent *r, intptr_t incr)
     }
   else 
     {
-	  retval = (void*)heap_ptr;
+      const size_t new_size = ((size_t)newbrk) - ((size_t)sheap_p);
+      heap_peak = (heap_peak < new_size) ? new_size : heap_peak;
+      retval = (void*)heap_ptr;
       heap_ptr = newbrk;
+      
     }
 
   return  retval;
